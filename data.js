5c3e6d9facc6149c13863c199ddda59e545afef9
Igorshk10 <igorshk271@gmail.com>
2025-11-05 16:02:50 +0200

    Додав масив з товарами

---
+++ data.js
@@ -0,0 +1,120 @@
+let data = [
+
+    // Red Bull Energy Drinks
+    
+    {
+        name: 'Оригінальний Red Bull',
+        price: 69,
+        raiting: 4.8,
+        currency: 'UAH',
+        url_img: 'https://www.redbull.com/energydrink/v1/resources/storyblok/images/f/287059/528x1348/159a46d829/ua_ed_250ml_energy-drink_country_rgb__cold_closed_front_com_full.png/m/616x0',
+        brand: 'Red Bull'
+    },
+    {
+        name: 'Red Bull Sugarfree',
+        price: 69,
+        currency: 'UAH',
+        rating: 4.7,
+        url_img: 'https://images.ctfassets.net/lcr8qbvxj7mh/1Z6HmWqrtJaIivBrQOpt9P/1f9398448fe950b7960a916d5688590b/UK_RBSF_250_Single-Unit_close_cold_ORIGINAL_canwidth528px.png',
+        brand: 'Red Bull'
+    },
+    {
+        name: 'Red Bull The Yellow Edition',
+        price: 72,
+        currency: 'UAH',
+        rating: 4.9,
+        url_img: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.redbull.com%2Fua-uk%2Fenergydrink%2Fproducts%2Fred-bull-yellow-edition&psig=AOvVaw0P2IRV_pRUnFryEoyhCFc_&ust=1762425180060000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCMiT4Mjn2pADFQAAAAAdAAAAABAE',
+        brand: 'Red Bull'
+    },
+    {
+        name: 'Red Bull The Red Edition',
+        price: 72,
+        currency: 'UAH',
+        rating: 4.9,
+        url_img: 'https://bevmo.com/cdn/shop/files/61351224-d028-4f9f-882e-c07927331fb8-background_removed.png?v=1741033520&width=990',
+        brand: 'Red Bull'
+    },
+    {
+        name: 'Red Bull The Amber Edition',
+        price: 72,
+        currency: 'UAH',
+        rating: 4.9,
+        url_img: 'https://bevmo.com/cdn/shop/files/5c890047-d0fa-4862-9c1d-79888beacb5a-background_removed.png?v=1741121639&width=990',
+        brand: 'Red Bull'
+    },
+    {
+        name: 'Оригінальний Red Bull 4x',
+        price: 260,
+        currency: 'UAH',
+        rating: 4.8,
+        url_img: 'https://bevmo.com/cdn/shop/files/c86f85ad-49f9-47a8-88de-49751412723f.png?v=1741033517&width=990',
+        brand: 'Red Bull'
+    },
+    {
+        name: 'Red Bull Sugarfree 4x',
+        price: 260,
+        currency: 'UAH',
+        rating: 4.7,
+        url_img: 'https://bevmo.com/cdn/shop/files/9a08d3fa-36bd-4842-b079-73092b72fa02.png?v=1741033521&width=990',
+        brand: 'Red Bull'
+    },
+    {
+        name: 'Red Bull The Yellow Edition 4x',
+        price: 275,
+        currency: 'UAH',
+        rating: 4.9,
+        url_img: 'https://bevmo.com/cdn/shop/files/d28321bb-2388-43fa-b8d3-b17302284683-background_removed.png?v=1741032435',
+        brand: 'Red Bull'
+    },
+    {
+        name: 'Red Bull The Amber Edition 4x',
+        price: 275,
+        currency: 'UAH',
+        rating: 4.9,
+        url_img: 'https://bevmo.com/cdn/shop/files/b5e36fc1-2685-4e36-aa84-3f3bc0d03fb5-background_removed.png?v=1747103658&width=990',
+        brand: 'Red Bull'
+    },
+
+    // Monster Energy Drinks
+
+    {
+        name: 'Monster Ultra Watermelon',
+        price: 75,
+        currency: 'UAH',
+        rating: 4.9,
+        url_img: 'https://bevmo.com/cdn/shop/files/0574a3ea-55e4-4425-a9dd-ca5b9aecc117_ff457fb6-be9c-4aef-ace4-7d9be2c3d4a2.png?v=1724304111&width=990',
+        brand: 'Monster'
+    },
+    {
+        name: 'Monster Energy Java Mean Bean',
+        price: 85,
+        currency: 'UAH',
+        rating: 4.8,
+        url_img: 'https://bevmo.com/cdn/shop/files/1e85d923-9fe8-4b12-9dd7-cf54a76b1be6.png?v=1737087870&width=990',
+        brand: 'Monster'
+    },
+    {
+        name: 'Monster Energy',
+        price: 75,
+        currency: 'UAH',
+        rating: 4.9,
+        url_img: 'https://bevmo.com/cdn/shop/files/eecc9dcb-024d-4f99-a586-5a744abec31f-background_removed.png?v=1738037375&width=990',
+        brand: 'Monster'
+    },
+    {
+        name: 'Monster Energy Zero Ultra',
+        price: 75,
+        currency: 'UAH',
+        rating: 4.8,
+        url_img: 'https://bevmo.com/cdn/shop/files/7583362c-e61d-4ce0-814a-e17c10a7846e-background_removed.png?v=1738038509&width=990',
+        brand: 'Monster'
+    },
+    {
+        name: 'Monster Energy Juice Khaotic',
+        price: 79,
+        currency: 'UAH',
+        rating: 4.9,
+        url_img: 'https://bevmo.com/cdn/shop/products/64e8c30a-49ad-4ee8-9187-02c377eea1df.png?v=1698879568&width=990',
+        brand: 'Monster'
+    }
+]
--- index.js
+++
@@ -1,8 +0,0 @@
-let data = [ {
-        name : "iPhone",
-        price : 699
-}, {
-        name : "Samsung Galaxy",
-        price : 599 
-}
-    ]
